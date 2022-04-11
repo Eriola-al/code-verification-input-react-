065b47b929520fc583208542bce88edff89e7da2
Eriola-al <endoj22@gmail.com>
2022-04-11 11:51:28 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [code, setCode] = useState(new Array(6).fill(""))
+  
+  const handleChange = (element, index) => {
+
+    setCode([...code.map((d,idx) => (idx === index) ? element.value : d)]);
+
+    if(element.nextSibling){
+      element.nextSibling.focus()
+    }
+  }
+  
+//   const deleteHandler = (e) => {
+//     let key = e.keyCode || e.charCode;
+
+//     if( key === 'Backspace' || key === 'Delete' )
+//         setCode("");
+// };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+    <h2 className='title'>Verify your account</h2>
+    <p className='subtitle'>We emailed you the six digit code to cool_guy@email.com
+      <br/>
+    Enter the code below to confirm your email address.</p>
+    {code.map((data,index) => {
+      return <input 
+      className="code-input"
+      type="text"
+      name="code"
+      maxLength="1"
+      key={index}
+      value={data}
+      placeholder={["0"-"0"-"0"-"0"-"0"-"0"]}
+      onChange={e => handleChange(e.target,index)}
+      onFocus={e => e.target.select()}
+      // onDelete={deleteHandler}
+      />
+    }) }
+    <div className='footer'>
+    <p className='endtitle'>This is design only. We didn't actually send you an email as we
+    <br/>
+    don't have your email, right?</p>
+    </div>
     </div>
   );
 }
